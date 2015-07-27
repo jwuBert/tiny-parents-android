cdf1a88f0e1320d3c1ffdee60e82bdbe7999acce
leechannl <leechannl@icloud.com>
2015-07-27 10:08:48 +0800

    feat (drawer): change drawer menu items click behavior
    start new activities instead of fragments when navigate to `about us`, `setting`, `switch child`

---
+++ app/src/main/java/com/cloudiya/app/tiny_parents_android/activity/SettingActivity.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2011-2015 Wuhan Cloudiya Tech. Co. Ltd
+ * This file is part of Little's Childhood （微童年） Software & APP
+ * Created by mm on 7/27/15.
+ *
+ * Purpose:
+ */
+
+package com.cloudiya.app.tiny_parents_android.activity;
+
+import android.app.Activity;
+
+public class SettingActivity extends Activity{
+}
