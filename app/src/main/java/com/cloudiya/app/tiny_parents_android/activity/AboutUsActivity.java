dd844bc49565dfdacefb1a5709e3f256405bb9fd
bert <348380751@qq.com>
2015-07-28 10:48:44 +0800

    finish AboutUsActivity

--- app/src/main/java/com/cloudiya/app/tiny_parents_android/activity/AboutUsActivity.java
+++ app/src/main/java/com/cloudiya/app/tiny_parents_android/activity/AboutUsActivity.java
@@ -9,6 +9,58 @@
 package com.cloudiya.app.tiny_parents_android.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.cloudiya.app.tiny_parents_android.R;
 
-public class AboutUsActivity extends Activity{
+	public class AboutUsActivity extends Activity {
+			private static  final  String TAG=AboutUsActivity.class.getSimpleName();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "pass AboutUsActivity");
+		setContentView(R.layout.activity_aboutus);
+		Button num1=(Button)findViewById(R.id.aboutus_service_tel1);
+ 		Button num2=(Button)findViewById(R.id.aboutus_service_tel2);
+ 		Button business=(Button)findViewById(R.id.aboutus_business_address);
+		Button skill=(Button)findViewById(R.id.aboutus_skill_address);
+
+		//call out
+		num1.setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick(View view) {
+
+				Uri uri = Uri.parse(getString(R.string.aboutus_telphonenumber1));
+				Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+				startActivity(intent);
+			}
+		});
+		num2.setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick(View view) {
+				Uri uri=Uri.parse(getString(R.string.aboutus_telphonenumber2));
+				Intent intent=new Intent(Intent.ACTION_DIAL,uri);
+				startActivity(intent);
+			}
+		});
+		//link worknet
+		business.setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick(View view) {
+ 				Uri uri=Uri.parse(getString(R.string.aboutus_sale_address));
+				Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+				startActivity(intent);
+			}
+		});
+		skill.setOnClickListener(new View.OnClickListener(){
+			@Override public void onClick(View view) {
+				Uri uri=Uri.parse(getString(R.string.aboutus_skill_address));
+				Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+				startActivity(intent);
+			}
+		});
+	}
 }
+
